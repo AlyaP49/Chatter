5378d416bd95ec58544e0c8dc8af3d752497fc8b
alya p <AlyaP2@Alyas-MacBook-Pro.local>
2018-01-13 15:28:57 -0600

    alldone

--- src/chatter/handler.clj
+++ src/chatter/handler.clj
@@ -2,12 +2,30 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
-            [hiccup.page :as page]))
+            [hiccup.page :as page]
+            [hiccup.form :as form]
+            [ring.middleware.params :refer [wrap-params]]))
 
 (def chat-messages
-  [{:name "blue" :message "hello world"}
-  {:name "red" :message "red is my faborite color"}
-  {:name "green" :message "green makes it go faster"}])
+  (atom ()))
+
+(defn generate-message-row
+  [message]
+  [:tr [:td (:name message)] [:td (:message message)]])
+
+(def message-form
+  [:p
+   (form/form-to
+     [:post "/"]
+     "Name:" (form/text-field "name")
+     "Message:" (form/text-field "msg")
+     (form/submit-button "Submit"))])
+
+(defn generate-message-table
+  [messages]
+  [:p
+    [:table
+      (map generate-message-row messages)]])
 
 (defn generate-messages-view
   "this generates the HTML for displaying the messages"
@@ -17,11 +35,20 @@
       [:title "chatter"]]
     [:body
       [:h1 {:style "color:green"} "Our Chat App"]
-      [:p messages]]))
+      message-form
+      (generate-message-table messages)]))
+
+(defn update-messages!
+  [messages new-name new-message]
+  (swap! messages conj
+    {:name new-name
+     :message new-message}))
+
 (defroutes app-routes
-  (GET "/" []
-    (generate-messages-view chat-messages))
+  (GET "/" [] (generate-messages-view @chat-messages))
+  (POST "/" [name msg]
+    (generate-messages-view
+     (update-messages! chat-messages name msg)))
   (route/not-found "Not Found"))
 
-(def app
-  (wrap-defaults app-routes site-defaults))
+(def app (wrap-params app-routes))
