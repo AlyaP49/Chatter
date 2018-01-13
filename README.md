87be865823f277f930c0f30055d99f97c08eb87e
alya p <AlyaP2@Alyas-MacBook-Pro.local>
2018-01-13 14:11:38 -0600

    late modifications

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # chatter
 
-Zorro
+Dr.P
 
 ## Prerequisites
 
@@ -16,4 +16,4 @@ To start a web server for the application, run:
 
 ## License
 
-Copyright © 2018 Zorro
+Copyright © 2018 Dr.P
--- src/chatter/handler.clj
+++ src/chatter/handler.clj
@@ -4,18 +4,23 @@
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [hiccup.page :as page]))
 
+(def chat-messages
+  [{:name "blue" :message "hello world"}
+  {:name "red" :message "red is my faborite color"}
+  {:name "green" :message "green makes it go faster"}])
+
 (defn generate-messages-view
   "this generates the HTML for displaying the messages"
-  []
+  [messages]
   (page/html5
     [:head
       [:title "chatter"]]
     [:body
-      [:h1 {:style "color:red"} "Our Chat App"]]))
-
+      [:h1 {:style "color:green"} "Our Chat App"]
+      [:p messages]]))
 (defroutes app-routes
   (GET "/" []
-    (generate-messages-view))
+    (generate-messages-view chat-messages))
   (route/not-found "Not Found"))
 
 (def app
