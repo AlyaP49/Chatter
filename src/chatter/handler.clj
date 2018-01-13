144d8509a6512e990c21f23b6ba47cea35294290
alya p <AlyaP2@Alyas-MacBook-Pro.local>
2018-01-13 13:47:45 -0600

    bonjour

--- src/chatter/handler.clj
+++ src/chatter/handler.clj
@@ -1,10 +1,21 @@
 (ns chatter.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [hiccup.page :as page]))
+
+(defn generate-messages-view
+  "this generates the HTML for displaying the messages"
+  []
+  (page/html5
+    [:head
+      [:title "chatter"]]
+    [:body
+      [:h1 {:style "color:red"} "Our Chat App"]]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" []
+    (generate-messages-view))
   (route/not-found "Not Found"))
 
 (def app
